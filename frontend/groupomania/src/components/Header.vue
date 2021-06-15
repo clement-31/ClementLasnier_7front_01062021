1049acf32bf24f24417342323a9e34d13ac31d17
Clement31 <clement-lasnier@orange.fr>
2021-06-15 16:09:29 +0200

    Ajout composant Header et modifs composant Footer

---
+++ frontend/groupomania/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <router-link class="nav-link text-white" to="/">
+      <img class="logo d-block mx-auto img-fluid p-1" src='@/assets/images/icon-left-font-monochrome-white-redimensionné.png' alt="logo Groupomania">
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+</style>
