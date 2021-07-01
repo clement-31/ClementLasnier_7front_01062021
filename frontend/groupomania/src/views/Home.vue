85195a191c22dded9fdbeda86a50f8cd6d881eaf
Clement31 <clement-lasnier@orange.fr>
2021-07-01 20:10:40 +0200

    Ajout .env et modif index.html

---
+++ frontend/groupomania/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <LoginForm v-if="!connected"/>
+
+    <Nav v-if="connected"/>
+    <NewPost v-if="connected"/>
+    <Post v-if="connected"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  components: {
+    LoginForm,
+    Post,
+    Nav,
+    NewPost
+  },
+  data(){
+    return{
+      connected:  true
+    };
+  },
+  created() {
+    this.connectionOk()
+  },
+  methods : {
+    connectionOk() {
+      if(localStorage.user !== undefined) {
+        this.connected = true;
+        console.log(('Vous êtes connecté!!'));
+      }
+      else if(localStorage.user == undefined) {
+        this.connected =  false;
+        console.log('Vous n\'êtes pas connecté !!');
+      }
+    }
+  }
+}
+</script>
+
